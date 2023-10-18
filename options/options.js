bc6dbf84a99746a5eaad3d5f4224c0a402640690
Taghogho Emefeke <tag@crowdbotics.com>
2023-10-18 11:13:47 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "samplesavetest-dev-123208",
   url: "https://samplesavetest-dev-123208.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
