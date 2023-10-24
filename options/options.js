d15084ad73fc2b5f25e542305a3bcc8451ae925f
Juan B <juan@crowdbotics.com>
2023-10-24 12:36:31 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "samplesavetest-dev-123208",
-  url: "https://samplesavetest-dev-123208.botics.co",
-  api: "https://samplesavetest-dev-123208.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "samplesavetest-dev-123208",
+  "url": "https://samplesavetest-dev-123208.botics.co",
+  "api": "https://samplesavetest-dev-123208.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
